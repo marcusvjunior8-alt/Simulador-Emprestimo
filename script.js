20eb4f98537c8549389c3d1449c2b0048e920870
Marcus <marcus.v.junior8@aluno.senai.br>
2026-09-18 17:03:30 -0300

    .

---
+++ script.js
@@ -0,0 +1,14 @@
+/* coleta informações */
+const nameclient = document.getElementById('namecliente').value
+const datebirth = document.getElementById('datebirth').value
+const cpf = document.getElementById('cpfcliente').value
+const loan = document.getElementById('loan').value
+const fees = document.getElementById('fees').value
+const installments = document.getElementById('installments').value
+let tentativa = 0
+function tentativa(){
+    tentativa++
+    if(tentativa < 3){
+        
+    }
+}
